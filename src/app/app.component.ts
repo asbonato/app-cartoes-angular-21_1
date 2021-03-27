5ff6612dda73c3bf733b8fcce02fd2e28de807ff
Sergio Bonato <asbonato@gmail.com>
2021-03-26 21:24:41 -0300

    apostila 09 de 2.4.4.4 até 2.4.6.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent {
   textoBotao = "Esconder";
   esconder =  false;
 
+  pessoas = [
+    {nome: "José", idade: 18},
+    {nome: "Maria", idade: 22}
+  ];
+
+  adicionar (nome: any, idade: any) {
+    this.pessoas = [...this.pessoas, {nome: nome, idade: idade}];
+  }
+
   alterarExibicao() {
     this.esconder = !this.esconder;
     this.textoBotao = this.esconder?"Exibir":"Esconder"; 

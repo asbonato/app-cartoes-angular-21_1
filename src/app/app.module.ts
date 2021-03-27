97b453afc356b7a8808a4d3f903e5b8b57f11928
Sergio Bonato <asbonato@gmail.com>
2021-03-27 00:06:43 -0300

    componente pessoa-cartao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PessoaCartaoComponent } from './pessoa-cartao/pessoa-cartao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PessoaCartaoComponent
   ],
   imports: [
     BrowserModule
